9b35e132b515555704048a55ff3cff8d3b2185b8
eal6gf <elum1234@gmail.com>
2018-02-28 18:44:32 -0500

    basic seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,14 @@ Manifest.destroy_all
 Item.destroy_all
 ManifestItem.destroy_all
 
+images = [
+  'https://i1.wp.com/freepngimages.com/wp-content/uploads/2016/02/blue-skater-skirt-transparent-background.png?resize=624%2C527',
+  'http://pngriver.com/wp-content/uploads/2017/11/Jacket-clothes-free-PNG-transparent-background-images-free-download-clipart-pics-strickfleece-steppjacke-rekordmeister-21959.png',
+  'http://www.pngmart.com/files/4/Floral-Dress-PNG-Photos.png',
+  'http://www.pngmart.com/files/1/Jacket-PNG-Clipart.png',
+  'http://www.pngmart.com/files/1/Jacket-PNG-Transparent-Image.png'
+]
+
 user = User.create(username: 'charlie' , password: 'monkeybusiness')
 20.times do
   Manifest.create(title: Faker::Lorem.word, limit: 20, description: Faker::Lorem.sentence, user_id: User.last.id)
@@ -20,7 +28,7 @@ end
 
 Manifest.all.each do |manifest|
   5.times do
-    item = Item.create(name: Faker::Lorem.word, description: Faker::Lorem.sentence, price: 5.00, brand: Faker::Lorem.word)
+    item = Item.create(name: Faker::Lorem.word, description: Faker::Lorem.sentence, price: 5.00, brand: Faker::Lorem.word, image: images.sample)
     manifest.items << item
   end
 end
